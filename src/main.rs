db18edb3e314a14ea9a457c06508cef4c08a52d9
Paul Gey <narpfel@gmx.de>
2023-07-13 22:02:38 +0200

    exit with exit code 1 on error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::process::exit;
+
 use bumpalo::Bump;
 
 fn main() {
     let bump = &Bump::new();
     let result = crabbing_interpreters::run(bump, std::env::args_os());
     if let Err(err) = result {
-        eprintln!("{err}");
+        eprintln!("{err:?}");
+        exit(1);
     }
 }
