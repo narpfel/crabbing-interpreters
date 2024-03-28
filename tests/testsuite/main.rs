f9cc0279b656ad1daa66083d6b5596d781c4799c
Paul Gey <narpfel@gmx.de>
2024-03-28 21:43:30 +0100

    add simple test that exercises the repl

---
+++ tests/testsuite/main.rs
@@ -0,0 +1,9 @@
+use std::process::Command;
+
+use insta_cmd::assert_cmd_snapshot;
+use insta_cmd::get_cargo_bin;
+
+#[test]
+fn repl_evaluates_expression() {
+    assert_cmd_snapshot!(Command::new(get_cargo_bin("crabbing-interpreters")).pass_stdin("1 + 2"))
+}
