dcc6003776c1cf5cd1fcc1882df135707d890768
Paul Gey <narpfel@gmx.de>
2024-03-30 19:17:35 +0100

    use path, not test name, as snapshot name for craftinginterpreters tests

--- tests/testsuite/craftinginterpreters.rs
+++ tests/testsuite/craftinginterpreters.rs
@@ -6,19 +6,15 @@ use insta_cmd::assert_cmd_snapshot;
 use insta_cmd::get_cargo_bin;
 use rstest::rstest;
 
-use crate::testname;
-
 fn as_relative(path: &Path) -> &Path {
     path.strip_prefix(env!("CARGO_MANIFEST_DIR")).unwrap()
 }
 
 #[rstest]
-fn operator_tests(
-    testname: String,
-    #[files("craftinginterpreters/test/operator/**/*.lox")] path: PathBuf,
-) {
+fn tests(#[files("craftinginterpreters/test/operator/**/*.lox")] path: PathBuf) {
+    let path = as_relative(&path);
     assert_cmd_snapshot!(
-        testname,
-        Command::new(get_cargo_bin("crabbing-interpreters")).arg(as_relative(&path))
+        path.display().to_string(),
+        Command::new(get_cargo_bin("crabbing-interpreters")).arg(path)
     )
 }
