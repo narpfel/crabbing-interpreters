af86867c8841f052ad8f81dd1ffb9434c2265b5b
Paul Gey <narpfel@gmx.de>
2024-03-31 12:41:06 +0200

    add craftinginterpreters logical_operator tests

--- tests/testsuite/craftinginterpreters.rs
+++ tests/testsuite/craftinginterpreters.rs
@@ -18,6 +18,7 @@ fn tests(
     #[files("craftinginterpreters/test/bool/**/*.lox")]
     #[files("craftinginterpreters/test/comments/**/*.lox")]
     #[files("craftinginterpreters/test/if/**/*.lox")]
+    #[files("craftinginterpreters/test/logical_operator/**/*.lox")]
     #[files("craftinginterpreters/test/nil/**/*.lox")]
     #[files("craftinginterpreters/test/number/**/*.lox")]
     #[files("craftinginterpreters/test/operator/**/*.lox")]
