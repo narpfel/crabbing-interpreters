afff867763a0fe54e2b524636deead26a71b6229
Paul Gey <narpfel@gmx.de>
2024-04-08 19:49:36 +0200

    add scope snapshot tests for all programs that can be scoped

--- crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
+++ crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
+use std::process::Stdio;
 
 use insta_cmd::assert_cmd_snapshot;
 use insta_cmd::get_cargo_bin;
@@ -40,4 +41,36 @@ fn tests(
             .arg(path)
             .current_dir("..")
     )
+}
+
+#[rstest]
+fn scope(
+    #[files("../craftinginterpreters/test/**/*.lox")]
+    #[files("tests/cases/**/*.lox")]
+    #[exclude("/benchmark/")]
+    #[exclude("loop_too_large\\.lox$")]
+    path: PathBuf,
+) {
+    let path = relative_to(
+        &path,
+        Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap(),
+    );
+    let command = || {
+        let mut command = Command::new(get_cargo_bin("crabbing-interpreters"));
+        command
+            .current_dir("..")
+            .arg(path)
+            .arg("--scopes")
+            .arg("--stop-at=scopes");
+        command
+    };
+    if command()
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .unwrap()
+        .success()
+    {
+        assert_cmd_snapshot!(format!("scope-{}", path.display()), command());
+    }
 }
