be8df0ba52ce5a41c2b395a7e8f5e5820785d76c
Paul Gey <narpfel@gmx.de>
2024-04-10 18:12:25 +0200

    add craftinginterpreters for loop tests

--- crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
+++ crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
@@ -19,6 +19,7 @@ fn tests(
     #[files("../craftinginterpreters/test/block/**/*.lox")]
     #[files("../craftinginterpreters/test/bool/**/*.lox")]
     #[files("../craftinginterpreters/test/comments/**/*.lox")]
+    #[files("../craftinginterpreters/test/for/**/*.lox")]
     #[files("../craftinginterpreters/test/function/**/*.lox")]
     #[files("../craftinginterpreters/test/if/**/*.lox")]
     #[files("../craftinginterpreters/test/logical_operator/**/*.lox")]
