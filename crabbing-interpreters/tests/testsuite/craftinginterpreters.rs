42b7268377959656ff0d23880a80b51aaf4082d6
Paul Gey <narpfel@gmx.de>
2024-04-19 18:49:00 +0200

    add craftinginterpreters limit tests

--- crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
+++ crabbing-interpreters/tests/testsuite/craftinginterpreters.rs
@@ -28,6 +28,7 @@ fn tests(
     #[files("../craftinginterpreters/test/function/**/*.lox")]
     #[files("../craftinginterpreters/test/if/**/*.lox")]
     #[files("../craftinginterpreters/test/inheritance/**/*.lox")]
+    #[files("../craftinginterpreters/test/limit/**/*.lox")]
     #[files("../craftinginterpreters/test/logical_operator/**/*.lox")]
     #[files("../craftinginterpreters/test/method/**/*.lox")]
     #[files("../craftinginterpreters/test/nil/**/*.lox")]
